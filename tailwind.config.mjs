ca466140747a33add8e2db906110a4d1be316431
Jared Aceves <gerardojaredaceves@gmail.com>
2025-04-01 11:04:08 -0600

    animacion boton toggle

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./app/**/*.{js,ts,jsx,tsx}",        // 👈 Scans pages
-      "./components/**/*.{js,ts,jsx,tsx}", // 👈 Scans your components
-    ],
-    theme: {
-      extend: {
-        colors: {
-          primary: "#092536",      // Navy
-          background: "#eaf0f2",   // Light blue-gray
-          accent: "#749bbb",       // Soft blue
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#092536",
+        background: "#eaf0f2",
+        accent: "#749bbb",
+      },
+      keyframes: {
+        slideDown: {
+          "0%": { opacity: 0, transform: "translateY(-10%)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
         },
-        keyframes:{
-            slideDown:{
-                '0%': { opacity: 0, transform: 'translateY(-10%)' },
-                '100%': { opacity: 1, transform: 'translateY(0)' },
-            }
-        }
-        
+      },
+      animation: {
+        slideDown: "slideDown 0.8s ease-out",
       },
     },
-    plugins: [],
-  };
-  
-  
+  },
+  plugins: [],
+};
