5d01ceb604cec14f6836fcc802eba524add16ad2
Jared Aceves <gerardojaredaceves@gmail.com>
2025-05-15 13:14:11 -0600

    por favor funciona

--- app/layout.js
+++ app/layout.js
@@ -7,9 +7,7 @@ import { AuthProvider } from '../context/AuthContext';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { headers } from 'next/headers';
-import OrganizationSchema from "./components/OrganizationSchema"
-import TikTokPixel from "./components/TiktokPixel";
-
+import OrganizationSchema from "./components/OrganizationSchema";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +20,10 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-
-    icons: {
-    icon: '/favicon.ico', // or whatever your favicon filename is
-    apple: '/apple-icon.png', // optional
-    },
-
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-icon.png',
+  },
   title: {
     default: "Aceves Joyería | Anillos y Collares con Alma",
     template: "%s | Aceves Joyería"
@@ -44,7 +40,6 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }) {
-  // Use this for canonical URL, but only on the client side
   const headersList = await headers();
   const path = headersList.get('x-pathname') || '';
   const canonicalUrl = `https://www.acevesoficial.com/${path}`;
@@ -54,7 +49,19 @@ export default async function RootLayout({ children }) {
       <head>
         <link rel="canonical" href={canonicalUrl} />
         <OrganizationSchema />
-         <TikTokPixel />
+        
+        {/* TikTok Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+                ttq.load('D0J3DIJC77U10T5VC50G');
+                ttq.page();
+              }(window, document, 'ttq');
+            `
+          }}
+        />
       </head>
       <body className="antialiased">
         <AuthProvider>
